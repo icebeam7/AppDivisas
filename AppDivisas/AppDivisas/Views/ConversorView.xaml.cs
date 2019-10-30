2f6b11920162f46533444a99704f9da5c4f5da4b
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-10-30 20:47:53 +0100

    Add project files.

---
+++ AppDivisas/AppDivisas/Views/ConversorView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDivisas.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConversorView : ContentPage
+    {
+        const double tasa = 19.11;
+
+        public ConversorView()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConversorButton_Clicked(object sender, EventArgs e)
+        {
+            double pesos = 0, dolares = 0;
+
+            if (double.TryParse(PesosEntry.Text, out pesos))
+            {
+                dolares = pesos / tasa;
+                DolaresLabel.Text = $"{dolares:N2}";
+            }
+            else
+            {
+                await DisplayAlert("Error", "Cantidad no válida", "OK");
+                Limpiar();
+            }
+        }
+
+        void Limpiar()
+        {
+            PesosEntry.Text = string.Empty;
+            DolaresLabel.Text = string.Empty;
+        }
+
+        private void LimpiarButton_Clicked(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+    }
+}
